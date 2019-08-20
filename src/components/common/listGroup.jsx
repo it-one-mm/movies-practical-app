f955835eb22bc129281296c923bb07744e92a3a4
it-one-mm <itonemm@gmail.com>
2019-08-20 12:44:02 +0630

    Handling Genre Selection

--- src/components/common/listGroup.jsx
+++ src/components/common/listGroup.jsx
@@ -1,13 +1,26 @@
 import React from "react";
 
 const ListGroup = props => {
-  const { items, textProperty, valueProperty } = props;
-  let classes = "list-group-item list-group-item-action";
+  const {
+    items,
+    textProperty,
+    valueProperty,
+    selectedItem,
+    onItemSelect
+  } = props;
 
   return (
     <div className="list-group">
       {items.map(item => (
-        <button key={item[valueProperty]} className={classes}>
+        <button
+          key={item[valueProperty]}
+          className={
+            selectedItem === item
+              ? "list-group-item list-group-item-action active"
+              : "list-group-item list-group-item-action"
+          }
+          onClick={() => onItemSelect(item)}
+        >
           {item[textProperty]}
         </button>
       ))}
